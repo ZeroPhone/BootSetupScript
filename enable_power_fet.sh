879b791cab6aff05da1d67f4a4295382fc3d9993
CRImier <crimier@yandex.ru>
2017-11-28 01:40:12 +0200

    Added waiting until the gpiochip*** file appears

--- enable_power_fet.sh
+++ enable_power_fet.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+#Ugly but working so far - wait until the GPIO chip file appears
+counter=0
+while [ ! -e /sys/class/gpio/gpiochip496 ]
+do
+  sleep 1
+  counter=`expr $counter + 1`
+  if [ $counter -gt 20 ]; then exit 1; fi
+done
+
 #Setting FET-controlling GPIO to low
 echo 511 > /sys/class/gpio/export || echo "Script already ran, or the IO expander is not available!"
 echo "low" > /sys/class/gpio/gpio511/direction
