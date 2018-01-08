a7c50d32e96bde1e805d16031adc5b8b4c3aff9c
root <root@zerophone.org>
2018-01-08 01:48:36 +0000

    Moved a file but forgot to adjust the script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 mkdir -p /opt/zerophone
 cp enable_power_fet.sh /opt/zerophone/
 chmod +x /opt/zerophone/enable_power_fet.sh
-cp enable_power_fet.service /etc/systemd/system
+cp debian/enable_power_fet.service /etc/systemd/system
 systemctl daemon-reload
 systemctl enable enable_power_fet.service
